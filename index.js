508c16578c6f7e7beee5844e78aeef5d4dcd8540
neeti-yaav <neetiy32@gmail.com>
2025-12-02 23:04:33 +0530

    2Dec2025:23:04

---
+++ index.js
@@ -0,0 +1,78 @@
+
+// console.log("I letm Neeti");
+
+
+
+// let a_g=0;
+
+
+//  console.log("up if");
+    
+// if(true){
+
+//     console.log("inside if",let);
+    
+// }
+// console.log("outside if ", let);
+
+
+
+let hello = "neetiYadav";
+console.log(hello);
+
+let a,b;
+console.log(a,b);
+
+let c=null;
+console.log(c);
+
+let d= "neeti"
+ d = 7;
+ console.log(d);
+
+
+ let person = {
+    firstName : "Milee",
+    age : 19
+ }
+ console.log(person.age, person.firstName);
+ console.log(person["age"],person["firstName"]);
+ let color
+ let fruit={
+    color:"red",
+    "name":"apple",
+    "shape":"round",
+    weight:"1kg",
+    "taste":"tasty"
+ }
+ 
+console.log(fruit.weight,color);
+
+let vegetable = {
+    size:"long",
+    shape:"rectangle",
+    color:"green",
+    weight:"500gm",
+}
+console.log(vegetable.color, vegetable.weight);
+
+
+let vehicles = {
+    color:"pink",
+    size:"big",
+    price:"500K",
+    weight:"200000kg",
+
+}
+
+console.log("the color of", vehicles.color ,"and the size is", vehicles.size, "and the price is", vehicles.price);
+
+
+let food = {
+    type:"chinese",
+    taste:"spicy",
+    price:"80rs",
+    location:"limbodi",
+    option:"pasta",
+}
+console.log(food.type, food.taste, food.price, food.location, food.option);
