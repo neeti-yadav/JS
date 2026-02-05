520e3bd83e684b32bb04723d2b85359c6dff82c7
neeti-yaav <neetiy32@gmail.com>
2026-02-05 23:55:06 +0530

    5Feb2026:23:54

---
+++ array.js
@@ -0,0 +1,83 @@
+
+// 
+// let heroes=["shaktiman","nagaarjun","superman",1,2]
+
+// console.log("arrY",heroes.length);
+// for( let i=0;i<heroes.length;i++){
+
+//     console.log("elements", heroes[i]);
+// }
+
+// let number_array=[ 1,2,3,4,5,6,7,8,9,10]
+
+// for( let i=0;i<number_array.length;i++){
+   
+//     // console.log("****",number_array[i]);
+
+
+//     if(number_array[i]%2==0){
+         
+//         console.log("Even Numbers", number_array[i]);
+//     }
+//     else{console.log("Odd Numbers", number_array[i]);
+
+//     }
+
+
+// }
+
+
+// let arrayValue = [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,]
+
+// for(let i=0; i<arrayValue.length; i++){
+//     if(arrayValue[i]===0){
+//         console.log("the value is true",arrayValue[i]);
+//     }
+//     else{
+//         console.log("the value is false",arrayValue[i]);
+//     }
+// }
+
+
+
+
+// let  coins=[1,2,5,10,20];
+
+// console.log("array of coins", coins);
+
+// console.log("20 rs ",coins[4] );
+
+// for( let i=0;i<coins.length;i++){
+
+//     console.log("coins", coins[i]);
+// }
+
+
+// let colours = ["Red","Black","Blue","Pink","NavyBlue","Purple","Orange","Green","White","Brown","Gray","Yellow"]
+// // console.log("Colour is:",colours[1]);
+
+// for(let i=0;i<colours.length;i++){
+//     console.log("All Colours:",colours[i]);
+// }
+
+
+// let numb=[1,2,3,4,5,6,7,8,9,10]
+
+// for(i=0;i<numb.length;i++){
+//         console.log("print the table of two",numb[i]*2);
+// }
+
+
+
+
+for(i=1;i<=10;i++){
+
+    let stars=""
+    for( let j=1;j<=i;j++){
+
+      
+        stars+="*"
+
+    }
+    console.log(stars);
+}
